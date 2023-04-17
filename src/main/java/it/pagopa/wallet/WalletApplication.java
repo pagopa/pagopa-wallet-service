fe361e0bf0cbaea40a3b30e19ed206580f2e00ab
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-04-17 17:42:29 +0200

    fix deploy pipeline

--- src/main/java/it/pagopa/wallet/WalletApplication.java
+++ src/main/java/it/pagopa/wallet/WalletApplication.java
@@ -1,12 +1,3 @@
-package it.pagopa.wallet;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class WalletApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WalletApplication.class, args);
-    }
 }
