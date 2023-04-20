55dc6cbe81d911517275152473c18299ef1a2883
Giovanni Berti <giovanni.berti@pagopa.it>
2023-04-20 15:57:53 +0200

    test(domain): add tests

--- src/test/kotlin/it/pagopa/wallet/domain/WalletTest.kt
+++ src/test/kotlin/it/pagopa/wallet/domain/WalletTest.kt
@@ -1,10 +1,19 @@
 package it.pagopa.wallet.domain
 
 import java.util.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class WalletTest {
+    @Test
+    fun `can construct wallet from UUID`() {
+        val walletId = WalletId(UUID.randomUUID())
+        val paymentInstruments = listOf(PaymentInstrument(PaymentInstrumentId(UUID.randomUUID())))
+        val wallet = Wallet(walletId, paymentInstruments)
+
+        assertEquals(walletId, wallet.walletId)
+    }
     @Test
     fun `wallet with empty payment instrument list is invalid`() {
         val paymentInstrumentList = listOf<PaymentInstrument>()
