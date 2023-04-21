b6dbda9e738e55b8aba0c08d5db08ea0d661f1bb
Giovanni Berti <giovanni.berti@pagopa.it>
2023-04-21 14:15:54 +0200

    refactor(converters): add warning suppression for id converters as functional interfaces

--- src/main/kotlin/it/pagopa/wallet/util/converters/mongo/PaymentInstrumentIdConverters.kt
+++ src/main/kotlin/it/pagopa/wallet/util/converters/mongo/PaymentInstrumentIdConverters.kt
@@ -6,6 +6,16 @@ import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.ReadingConverter
 import org.springframework.data.convert.WritingConverter
 
+/*
+ * @formatter:off
+ *
+ * Warning kotlin:S6516 - Functional interface implementations should use lambda expressions
+ * Suppressed to allow for this converter implementation to be tested and not to harm
+ * readability when configuring converters (see it.pagopa.wallet.config.MongoConfiguration#mongoCustomConversions)
+ *
+ * @formatter:on
+ */
+@SuppressWarnings("kotlin:S6516")
 @WritingConverter
 object PaymentInstrumentIdWriter : Converter<PaymentInstrumentId, String> {
     override fun convert(source: PaymentInstrumentId): String {
@@ -13,6 +23,16 @@ object PaymentInstrumentIdWriter : Converter<PaymentInstrumentId, String> {
     }
 }
 
+/*
+ * @formatter:off
+ *
+ * Warning kotlin:S6516 - Functional interface implementations should use lambda expressions
+ * Suppressed to allow for this converter implementation to be tested and not to harm
+ * readability when configuring converters (see it.pagopa.wallet.config.MongoConfiguration#mongoCustomConversions)
+ *
+ * @formatter:on
+ */
+@SuppressWarnings("kotlin:S6516")
 @ReadingConverter
 object PaymentInstrumentIdReader : Converter<String, PaymentInstrumentId> {
     override fun convert(source: String): PaymentInstrumentId {
--- src/main/kotlin/it/pagopa/wallet/util/converters/mongo/WalletIdConverters.kt
+++ src/main/kotlin/it/pagopa/wallet/util/converters/mongo/WalletIdConverters.kt
@@ -6,6 +6,16 @@ import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.ReadingConverter
 import org.springframework.data.convert.WritingConverter
 
+/*
+ * @formatter:off
+ *
+ * Warning kotlin:S6516 - Functional interface implementations should use lambda expressions
+ * Suppressed to allow for this converter implementation to be tested and not to harm
+ * readability when configuring converters (see it.pagopa.wallet.config.MongoConfiguration#mongoCustomConversions)
+ *
+ * @formatter:on
+ */
+@SuppressWarnings("kotlin:S6516")
 @WritingConverter
 object WalletIdWriter : Converter<WalletId, String> {
     override fun convert(source: WalletId): String {
@@ -13,6 +23,16 @@ object WalletIdWriter : Converter<WalletId, String> {
     }
 }
 
+/*
+ * @formatter:off
+ *
+ * Warning kotlin:S6516 - Functional interface implementations should use lambda expressions
+ * Suppressed to allow for this converter implementation to be tested and not to harm
+ * readability when configuring converters (see it.pagopa.wallet.config.MongoConfiguration#mongoCustomConversions)
+ *
+ * @formatter:on
+ */
+@SuppressWarnings("kotlin:S6516")
 @ReadingConverter
 object WalletIdReader : Converter<String, WalletId> {
     override fun convert(source: String): WalletId {
