ebf097af4f5f77b7ad8a45cbcb951a9781108579
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-05-05 12:27:30 +0200

    update notify url

--- src/main/kotlin/it/pagopa/wallet/services/WalletService.kt
+++ src/main/kotlin/it/pagopa/wallet/services/WalletService.kt
@@ -13,143 +13,142 @@ import it.pagopa.wallet.domain.details.CardDetails
 import it.pagopa.wallet.domain.details.WalletDetails
 import it.pagopa.wallet.exception.*
 import it.pagopa.wallet.repositories.WalletRepository
+import java.net.URI
+import java.time.OffsetDateTime
+import java.util.*
 import kotlinx.coroutines.reactor.awaitSingle
 import lombok.extern.slf4j.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
-import java.net.URI
-import java.time.OffsetDateTime
-import java.util.*
 
 @Service
 @Slf4j
 class WalletService(
-        @Autowired private val walletRepository: WalletRepository,
-        @Autowired private val npgClient: NpgClient
+    @Autowired private val walletRepository: WalletRepository,
+    @Autowired private val npgClient: NpgClient
 ) {
     fun createWallet(
-            walletCreateRequestDto: WalletCreateRequestDto,
-            userId: String
+        walletCreateRequestDto: WalletCreateRequestDto,
+        userId: String
     ): Mono<Pair<Wallet, URI>> {
         val contractNumber = generateRandomString(18)
         return npgClient
-                .orderHpp(
-                        UUID.randomUUID(),
-                        HppRequest().apply {
-                            order =
-                                    OrderItem().apply {
-                                        orderId = generateRandomString(27)
-                                        amount = 0.toString()
-                                        currency = "EUR"
-                                        customerId = generateRandomString(27)
-                                    }
-                            paymentSession =
-                                    PaymentSessionItem().apply {
-                                        amount = 0.toString()
-                                        language = "ita"
-                                        resultUrl = URI.create("http://localhost")
-                                        cancelUrl = URI.create("http://localhost")
-                                        notificationUrl =
-                                                URI.create(
-                                                        "https://api.dev.platform.pagopa.it/wallet-notifications-service/v1/notify"
-                                                )
-                                        paymentService = PaymentSessionItem.PaymentServiceEnum.CARDS
-                                        actionType = PaymentSessionItem.ActionTypeEnum.VERIFY
-                                        recurrence =
-                                                RecurrenceItem().apply {
-                                                    action = RecurrenceItem.ActionEnum.CONTRACT_CREATION
-                                                    contractId = contractNumber
-                                                    contractType = RecurrenceItem.ContractTypeEnum.CIT
-                                                }
-                                    }
+            .orderHpp(
+                UUID.randomUUID(),
+                HppRequest().apply {
+                    order =
+                        OrderItem().apply {
+                            orderId = generateRandomString(27)
+                            amount = 0.toString()
+                            currency = "EUR"
+                            customerId = generateRandomString(27)
                         }
-                )
-                .onErrorMap {
-                    BadGatewayException(
-                            "Could not send order to payment gateway. Reason: ${it.message}"
-                    )
-                            .initCause(it)
-                }
-                .map {
-                    val securityToken =
-                            it.securityToken
-                                    ?: throw BadGatewayException(
-                                            "Payment gateway didn't provide security token"
-                                    )
-                    val redirectUrl =
-                            it.hostedPage
-                                    ?: throw BadGatewayException("Payment gateway didn't provide redirect URL")
-                    Pair(securityToken, redirectUrl)
-                }
-                .flatMap { (securityToken, redirectUrl) ->
-                    val now = OffsetDateTime.now().toString()
-
-                    // TODO: update null values
-                    val wallet =
-                            Wallet(
-                                    WalletId(UUID.randomUUID()),
-                                    userId,
-                                    WalletStatusDto.INITIALIZED,
-                                    now,
-                                    now,
-                                    walletCreateRequestDto.type,
-                                    null,
-                                    securityToken,
-                                    walletCreateRequestDto.services,
-                                    contractNumber,
-                                    null
-                            )
-
-                    walletRepository
-                            .save(wallet)
-                            .map { savedWallet -> Pair(savedWallet, URI.create(redirectUrl)) }
-                            .onErrorMap {
-                                InternalServerErrorException(
-                                        "Error saving wallet to DB, ${it.localizedMessage}"
+                    paymentSession =
+                        PaymentSessionItem().apply {
+                            amount = 0.toString()
+                            language = "ita"
+                            resultUrl = URI.create("http://localhost")
+                            cancelUrl = URI.create("http://localhost")
+                            notificationUrl =
+                                URI.create(
+                                    "https://api.dev.platform.pagopa.it/wallet-notifications-service/v1/notify"
                                 )
-                            }
+                            paymentService = PaymentSessionItem.PaymentServiceEnum.CARDS
+                            actionType = PaymentSessionItem.ActionTypeEnum.VERIFY
+                            recurrence =
+                                RecurrenceItem().apply {
+                                    action = RecurrenceItem.ActionEnum.CONTRACT_CREATION
+                                    contractId = contractNumber
+                                    contractType = RecurrenceItem.ContractTypeEnum.CIT
+                                }
+                        }
                 }
+            )
+            .onErrorMap {
+                BadGatewayException(
+                        "Could not send order to payment gateway. Reason: ${it.message}"
+                    )
+                    .initCause(it)
+            }
+            .map {
+                val securityToken =
+                    it.securityToken
+                        ?: throw BadGatewayException(
+                            "Payment gateway didn't provide security token"
+                        )
+                val redirectUrl =
+                    it.hostedPage
+                        ?: throw BadGatewayException("Payment gateway didn't provide redirect URL")
+                Pair(securityToken, redirectUrl)
+            }
+            .flatMap { (securityToken, redirectUrl) ->
+                val now = OffsetDateTime.now().toString()
+
+                // TODO: update null values
+                val wallet =
+                    Wallet(
+                        WalletId(UUID.randomUUID()),
+                        userId,
+                        WalletStatusDto.INITIALIZED,
+                        now,
+                        now,
+                        walletCreateRequestDto.type,
+                        null,
+                        securityToken,
+                        walletCreateRequestDto.services,
+                        contractNumber,
+                        null
+                    )
+
+                walletRepository
+                    .save(wallet)
+                    .map { savedWallet -> Pair(savedWallet, URI.create(redirectUrl)) }
+                    .onErrorMap {
+                        InternalServerErrorException(
+                            "Error saving wallet to DB, ${it.localizedMessage}"
+                        )
+                    }
+            }
     }
 
     fun getWallet(walletId: UUID): Mono<WalletInfoDto> =
-            walletRepository
-                    .findById(walletId.toString())
-                    .switchIfEmpty(Mono.error(WalletNotFoundException(WalletId(walletId))))
-                    .map {
-                        WalletInfoDto()
-                                .walletId(it.id.value)
-                                .userId(it.userId)
-                                .status(it.status)
-                                .creationDate(OffsetDateTime.parse(it.creationDate))
-                                .updateDate(OffsetDateTime.parse(it.updateDate))
-                                .paymentInstrumentId(it.paymentInstrumentId?.value.toString())
-                                .services(it.services)
-                                .contractNumber(it.contractNumber)
-                                .details(buildWalletInfoDetails(it.details))
-                    }
+        walletRepository
+            .findById(walletId.toString())
+            .switchIfEmpty(Mono.error(WalletNotFoundException(WalletId(walletId))))
+            .map {
+                WalletInfoDto()
+                    .walletId(it.id.value)
+                    .userId(it.userId)
+                    .status(it.status)
+                    .creationDate(OffsetDateTime.parse(it.creationDate))
+                    .updateDate(OffsetDateTime.parse(it.updateDate))
+                    .paymentInstrumentId(it.paymentInstrumentId?.value.toString())
+                    .services(it.services)
+                    .contractNumber(it.contractNumber)
+                    .details(buildWalletInfoDetails(it.details))
+            }
 
     private fun buildWalletInfoDetails(walletDetails: WalletDetails?): WalletInfoDetailsDto? =
-            if (walletDetails != null) {
-                when (walletDetails) {
-                    is CardDetails ->
-                        WalletCardDetailsDto()
-                                .type(TypeDto.CARDS.toString())
-                                .bin(walletDetails.bin)
-                                .maskedPan(walletDetails.maskedPan)
-                                .expiryDate(walletDetails.expiryDate)
-                                .brand(walletDetails.brand)
-                                .holder(walletDetails.holderName)
-
-                    else -> {
-                        throw InternalServerErrorException(
-                                "Unhandled fetched wallet details of type ${walletDetails.javaClass}"
-                        )
-                    }
+        if (walletDetails != null) {
+            when (walletDetails) {
+                is CardDetails ->
+                    WalletCardDetailsDto()
+                        .type(TypeDto.CARDS.toString())
+                        .bin(walletDetails.bin)
+                        .maskedPan(walletDetails.maskedPan)
+                        .expiryDate(walletDetails.expiryDate)
+                        .brand(walletDetails.brand)
+                        .holder(walletDetails.holderName)
+                else -> {
+                    throw InternalServerErrorException(
+                        "Unhandled fetched wallet details of type ${walletDetails.javaClass}"
+                    )
                 }
-            } else {
-                null
             }
+        } else {
+            null
+        }
 
     private fun generateRandomString(length: Int): String {
         val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
@@ -159,22 +158,22 @@ class WalletService(
     // TODO Do we need to check if wallet status is initialized?
     suspend fun notify(correlationId: UUID, notification: NotificationRequestDto): Wallet {
         return walletRepository
-                .findByContractNumber(notification.contractId!!)
-                .switchIfEmpty(Mono.error(ContractIdNotFoundException()))
-                .filter { w -> w.gatewaySecurityToken == notification.securityToken }
-                .switchIfEmpty(Mono.error(SecurityTokenMatchException()))
-                .flatMap { wallet ->
-                    walletRepository.save(
-                            wallet.apply { wallet.status = getWalletStatus(notification.status) }
-                    )
-                }
-                .awaitSingle()
+            .findByContractNumber(notification.contractId!!)
+            .switchIfEmpty(Mono.error(ContractIdNotFoundException()))
+            .filter { w -> w.gatewaySecurityToken == notification.securityToken }
+            .switchIfEmpty(Mono.error(SecurityTokenMatchException()))
+            .flatMap { wallet ->
+                walletRepository.save(
+                    wallet.apply { wallet.status = getWalletStatus(notification.status) }
+                )
+            }
+            .awaitSingle()
     }
 
     private fun getWalletStatus(status: NotificationRequestDto.Status?): WalletStatusDto =
-            if (status == NotificationRequestDto.Status.OK) {
-                WalletStatusDto.CREATED
-            } else {
-                WalletStatusDto.ERROR
-            }
+        if (status == NotificationRequestDto.Status.OK) {
+            WalletStatusDto.CREATED
+        } else {
+            WalletStatusDto.ERROR
+        }
 }
