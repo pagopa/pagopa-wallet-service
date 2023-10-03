74fbb510e5fe359c64d5e83ee7485496f78a6a5f
Giovanni Berti <giovanni.berti@pagopa.it>
2023-10-03 14:44:33 +0200

    feat(service): add service repository

--- src/main/kotlin/it/pagopa/wallet/domain/services/Service.kt
+++ src/main/kotlin/it/pagopa/wallet/domain/services/Service.kt
@@ -1,7 +1,10 @@
 package it.pagopa.wallet.domain.services
 
+import java.time.Instant
+
 data class Service(
     val id: ServiceId,
     val name: ServiceName,
-    val status: ServiceStatus
+    val status: ServiceStatus,
+    val lastUpdated: Instant
 )
