efad29402e2498435305e4727b09b7c3cb76b090
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-10-13 11:01:31 +0200

    fix spotless

--- src/main/kotlin/it/pagopa/wallet/documents/wallets/Wallet.kt
+++ src/main/kotlin/it/pagopa/wallet/documents/wallets/Wallet.kt
@@ -4,39 +4,39 @@ import it.pagopa.generated.wallet.model.WalletStatusDto
 import it.pagopa.wallet.documents.wallets.details.WalletDetails
 import it.pagopa.wallet.domain.wallets.*
 import it.pagopa.wallet.domain.wallets.Wallet
-import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 import java.util.*
+import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("wallets")
 data class Wallet(
-        val id: String,
-        val userId: String,
-        val status: String,
-        val creationDate: String,
-        val updateDate: String,
-        val paymentMethodId: String,
-        val paymentInstrumentId: String?,
-        val contractId: String,
-        val applications: List<Application>,
-        val details: WalletDetails<*>?
+    val id: String,
+    val userId: String,
+    val status: String,
+    val creationDate: String,
+    val updateDate: String,
+    val paymentMethodId: String,
+    val paymentInstrumentId: String?,
+    val contractId: String,
+    val applications: List<Application>,
+    val details: WalletDetails<*>?
 ) {
 
     fun setApplications(
-            applications: List<Application>
+        applications: List<Application>
     ): it.pagopa.wallet.documents.wallets.Wallet = this.copy(applications = applications)
 
     fun toDomain() =
-            Wallet(
-                    WalletId(UUID.fromString(id)),
-                    UserId(UUID.fromString(userId)),
-                    WalletStatusDto.CREATED,
-                    Instant.parse(creationDate),
-                    Instant.parse(updateDate),
-                    PaymentMethodId(UUID.fromString(paymentMethodId)),
-                    paymentInstrumentId?.let { PaymentInstrumentId(UUID.fromString(it)) },
-                    applications.map { application -> application.toDomain() },
-                    ContractId(contractId),
-                    details?.toDomain()
-            )
+        Wallet(
+            WalletId(UUID.fromString(id)),
+            UserId(UUID.fromString(userId)),
+            WalletStatusDto.CREATED,
+            Instant.parse(creationDate),
+            Instant.parse(updateDate),
+            PaymentMethodId(UUID.fromString(paymentMethodId)),
+            paymentInstrumentId?.let { PaymentInstrumentId(UUID.fromString(it)) },
+            applications.map { application -> application.toDomain() },
+            ContractId(contractId),
+            details?.toDomain()
+        )
 }
