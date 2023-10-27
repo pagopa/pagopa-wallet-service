30b187c160edef29b27700a2e69a9873acce9ab8
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2023-10-28 00:35:19 +0200

    Update src/main/kotlin/it/pagopa/wallet/exception/ConfirmPaymentException.kt
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- src/main/kotlin/it/pagopa/wallet/exception/ConfirmPaymentException.kt
+++ src/main/kotlin/it/pagopa/wallet/exception/ConfirmPaymentException.kt
@@ -6,5 +6,5 @@ import org.springframework.http.HttpStatus
 class ConfirmPaymentException(walletId: WalletId) :
     ApiError("Confirm Payment didn't get right state for wallet with id $walletId") {
     override fun toRestException(): RestApiException =
-        RestApiException(HttpStatus.CONFLICT, "Wallet and session mismatch", message!!)
+        RestApiException(HttpStatus.CONFLICT, "Wrong state for confirm payment", message!!)
 }
