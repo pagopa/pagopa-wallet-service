7735044d8645fd9de4951fbcf5cc488736fae2d3
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-11-08 17:01:50 +0100

    fix spotless

--- src/main/kotlin/it/pagopa/wallet/controllers/WalletController.kt
+++ src/main/kotlin/it/pagopa/wallet/controllers/WalletController.kt
@@ -7,6 +7,8 @@ import it.pagopa.wallet.domain.services.ServiceStatus
 import it.pagopa.wallet.repositories.LoggingEventRepository
 import it.pagopa.wallet.services.WalletService
 import it.pagopa.wallet.util.UniqueIdUtils
+import java.net.URI
+import java.util.*
 import kotlinx.coroutines.reactor.mono
 import lombok.extern.slf4j.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,77 +20,75 @@ import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.util.UriComponentsBuilder
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import java.net.URI
-import java.util.*
 
 @RestController
 @Slf4j
 @Validated
 class WalletController(
-        @Autowired private val walletService: WalletService,
-        @Autowired private val loggingEventRepository: LoggingEventRepository,
-        @Value("\${webview.payment-wallet}") private val webviewPaymentWalletUrl: URI,
-        @Autowired private val uniqueIdUtils: UniqueIdUtils
+    @Autowired private val walletService: WalletService,
+    @Autowired private val loggingEventRepository: LoggingEventRepository,
+    @Value("\${webview.payment-wallet}") private val webviewPaymentWalletUrl: URI,
+    @Autowired private val uniqueIdUtils: UniqueIdUtils
 ) : WalletsApi {
 
     override fun createWallet(
-            xUserId: UUID,
-            walletCreateRequestDto: Mono<WalletCreateRequestDto>,
-            exchange: ServerWebExchange
+        xUserId: UUID,
+        walletCreateRequestDto: Mono<WalletCreateRequestDto>,
+        exchange: ServerWebExchange
     ): Mono<ResponseEntity<WalletCreateResponseDto>> {
 
         return walletCreateRequestDto
-                .flatMap { request ->
-                    walletService
-                            .createWallet(
-                                    request.services.map { s -> ServiceName(s.name) },
-                                    userId = xUserId,
-                                    paymentMethodId = request.paymentMethodId
-                            )
-                            .flatMap { it.saveEvents(loggingEventRepository) }
-                            .map { it.id.value to request.useDiagnosticTracing }
-                }
-                .map {
-                    WalletCreateResponseDto()
-                            .redirectUrl(
-                                    UriComponentsBuilder.fromUri(webviewPaymentWalletUrl.toURL().toURI())
-                                            .fragment("walletId=${it.first}&useDiagnosticTracing=${it.second}")
-                                            .build()
-                                            .toUriString()
-                            )
-                }
-                .map { ResponseEntity.created(URI.create(it.redirectUrl)).body(it) }
+            .flatMap { request ->
+                walletService
+                    .createWallet(
+                        request.services.map { s -> ServiceName(s.name) },
+                        userId = xUserId,
+                        paymentMethodId = request.paymentMethodId
+                    )
+                    .flatMap { it.saveEvents(loggingEventRepository) }
+                    .map { it.id.value to request.useDiagnosticTracing }
+            }
+            .map {
+                WalletCreateResponseDto()
+                    .redirectUrl(
+                        UriComponentsBuilder.fromUri(webviewPaymentWalletUrl.toURL().toURI())
+                            .fragment("walletId=${it.first}&useDiagnosticTracing=${it.second}")
+                            .build()
+                            .toUriString()
+                    )
+            }
+            .map { ResponseEntity.created(URI.create(it.redirectUrl)).body(it) }
     }
 
     override fun createSessionWallet(
-            walletId: UUID,
-            exchange: ServerWebExchange?
+        walletId: UUID,
+        exchange: ServerWebExchange?
     ): Mono<ResponseEntity<SessionWalletCreateResponseDto>> {
         val orderId = uniqueIdUtils.generateUniqueId()
         return walletService
-                .createSessionWallet(walletId, orderId)
-                .flatMap { (fields, walletEvent) ->
-                    walletEvent.saveEvents(loggingEventRepository).map { fields }
-                }
-                .map {
-                    ResponseEntity.ok()
-                            .body(
-                                    SessionWalletCreateResponseDto()
-                                            .orderId(orderId)
-                                            .cardFormFields(
-                                                    it.fields
-                                                            .stream()
-                                                            .map { f ->
-                                                                FieldDto()
-                                                                        .id(f.id)
-                                                                        .src(URI.create(f.src))
-                                                                        .type(f.type)
-                                                                        .propertyClass(f.propertyClass)
-                                                            }
-                                                            .toList()
-                                            )
+            .createSessionWallet(walletId, orderId)
+            .flatMap { (fields, walletEvent) ->
+                walletEvent.saveEvents(loggingEventRepository).map { fields }
+            }
+            .map {
+                ResponseEntity.ok()
+                    .body(
+                        SessionWalletCreateResponseDto()
+                            .orderId(orderId)
+                            .cardFormFields(
+                                it.fields
+                                    .stream()
+                                    .map { f ->
+                                        FieldDto()
+                                            .id(f.id)
+                                            .src(URI.create(f.src))
+                                            .type(f.type)
+                                            .propertyClass(f.propertyClass)
+                                    }
+                                    .toList()
                             )
-                }
+                    )
+            }
     }
 
     /*
@@ -103,23 +103,23 @@ class WalletController(
      */
     @SuppressWarnings("kotlin:S6508")
     override fun deleteWalletById(
-            walletId: UUID,
-            exchange: ServerWebExchange
+        walletId: UUID,
+        exchange: ServerWebExchange
     ): Mono<ResponseEntity<Void>> {
         // TODO To be implemented
         return mono { ResponseEntity.noContent().build() }
     }
 
     override fun getWalletById(
-            walletId: UUID,
-            exchange: ServerWebExchange
+        walletId: UUID,
+        exchange: ServerWebExchange
     ): Mono<ResponseEntity<WalletInfoDto>> {
         return walletService.findWallet(walletId).map { ResponseEntity.ok(it) }
     }
 
     override fun getWalletsByIdUser(
-            xUserId: UUID,
-            exchange: ServerWebExchange
+        xUserId: UUID,
+        exchange: ServerWebExchange
     ): Mono<ResponseEntity<WalletsDto>> {
         return walletService.findWalletByUserId(xUserId).map { ResponseEntity.ok(it) }
     }
@@ -136,29 +136,30 @@ class WalletController(
      */
     @SuppressWarnings("kotlin:S6508")
     override fun patchWalletById(
-            walletId: UUID,
-            patchServiceDto: Flux<PatchServiceDto>,
-            exchange: ServerWebExchange
+        walletId: UUID,
+        patchServiceDto: Flux<PatchServiceDto>,
+        exchange: ServerWebExchange
     ): Mono<ResponseEntity<Void>> {
 
         return patchServiceDto
-                .flatMap {
-                    walletService.patchWallet(
-                            walletId,
-                            Pair(ServiceName(it.name.name), ServiceStatus.valueOf(it.status.value))
-                    )
-                }
-                .flatMap { it.saveEvents(loggingEventRepository) }
-                .collectList()
-                .map { ResponseEntity.noContent().build() }
+            .flatMap {
+                walletService.patchWallet(
+                    walletId,
+                    Pair(ServiceName(it.name.name), ServiceStatus.valueOf(it.status.value))
+                )
+            }
+            .flatMap { it.saveEvents(loggingEventRepository) }
+            .collectList()
+            .map { ResponseEntity.noContent().build() }
     }
 
     override fun postWalletValidations(
-            walletId: UUID,
-            orderId: String,
-            exchange: ServerWebExchange
+        walletId: UUID,
+        orderId: String,
+        exchange: ServerWebExchange
     ): Mono<ResponseEntity<WalletVerifyRequestsResponseDto>> {
-        return walletService.validateWalletSession(orderId, walletId).flatMap { (response, walletEvent) ->
+        return walletService.validateWalletSession(orderId, walletId).flatMap {
+            (response, walletEvent) ->
             walletEvent.saveEvents(loggingEventRepository).map {
                 ResponseEntity.ok().body(response)
             }
