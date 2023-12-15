0f33c8ef9338d11d4e25a95dc3387c0080bfa864
Pietro Tota <pietro.tota@pagopa.it>
2023-12-15 14:49:12 +0100

    fix: spotless

--- src/main/kotlin/it/pagopa/wallet/client/NpgClient.kt
+++ src/main/kotlin/it/pagopa/wallet/client/NpgClient.kt
@@ -5,6 +5,7 @@ import it.pagopa.generated.npg.api.PaymentServicesApi
 import it.pagopa.generated.npg.model.*
 import it.pagopa.wallet.domain.details.WalletDetailsType
 import it.pagopa.wallet.exception.NpgClientException
+import java.util.*
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
@@ -13,7 +14,6 @@ import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
-import java.util.*
 
 /** NPG API client service class */
 @Component
@@ -33,11 +33,9 @@ class NpgClient(
                 WalletDetailsType.CARDS.name -> {
                     cardsServicesApi
                 }
-
                 WalletDetailsType.PAYPAL.name -> {
                     paypalServicesApi
                 }
-
                 else ->
                     throw NpgClientException(
                         "Invalid /order/build request: unhandled `paymentSession.paymentService` (value is ${createHostedOrderRequest.paymentSession?.paymentService})",
@@ -48,8 +46,9 @@ class NpgClient(
         val response: Mono<Fields> =
             try {
                 logger.info("Sending orderBuild with correlationId: $correlationId")
-                client.pspApiV1OrdersBuildPost(correlationId, createHostedOrderRequest)
-                    .doOnNext { logger.info("NPG build response: ${ObjectMapper().writeValueAsString(it)}") }
+                client.pspApiV1OrdersBuildPost(correlationId, createHostedOrderRequest).doOnNext {
+                    logger.info("NPG build response: ${ObjectMapper().writeValueAsString(it)}")
+                }
             } catch (e: WebClientResponseException) {
                 Mono.error(e)
             }
@@ -110,19 +109,16 @@ class NpgClient(
                     description = "Bad request",
                     httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR,
                 )
-
             HttpStatus.UNAUTHORIZED ->
                 NpgClientException(
                     description = "Misconfigured NPG api key",
                     httpStatusCode = HttpStatus.INTERNAL_SERVER_ERROR,
                 )
-
             HttpStatus.INTERNAL_SERVER_ERROR ->
                 NpgClientException(
                     description = "NPG internal server error",
                     httpStatusCode = HttpStatus.BAD_GATEWAY,
                 )
-
             else ->
                 NpgClientException(
                     description = "NPG server error: $statusCode",
