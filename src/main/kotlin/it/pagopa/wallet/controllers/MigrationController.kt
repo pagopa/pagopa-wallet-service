e38b8b1cba8f0352a5d8d3be55ccfe661a3df43b
petretiandrea <petretiandrea@gmail.com>
2024-02-21 15:56:45 +0100

    added fake controller

--- src/main/kotlin/it/pagopa/wallet/controllers/MigrationController.kt
+++ src/main/kotlin/it/pagopa/wallet/controllers/MigrationController.kt
@@ -3,15 +3,14 @@ package it.pagopa.wallet.controllers
 import it.pagopa.generated.wallet.api.MigrationsApi
 import it.pagopa.generated.wallet.model.WalletPmAssociationRequestDto
 import it.pagopa.generated.wallet.model.WalletPmAssociationResponseDto
-import it.pagopa.wallet.repositories.LoggingEventRepository
-import it.pagopa.wallet.services.WalletService
+import it.pagopa.generated.wallet.model.WalletStatusDto
 import lombok.extern.slf4j.Slf4j
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
+import java.util.*
 
 @RestController
 @Slf4j
@@ -21,6 +20,11 @@ class MigrationController : MigrationsApi {
         walletPmAssociationRequestDto: Mono<WalletPmAssociationRequestDto>?,
         exchange: ServerWebExchange?
     ): Mono<ResponseEntity<WalletPmAssociationResponseDto>> {
-        TODO("Not yet implemented")
+        return WalletPmAssociationResponseDto()
+            .walletIdPm(123)
+            .walletId(UUID.randomUUID())
+            .contractId("contractId")
+            .status(WalletStatusDto.CREATED)
+            .let { Mono.just(ResponseEntity.ofNullable(it)) }
     }
 }
