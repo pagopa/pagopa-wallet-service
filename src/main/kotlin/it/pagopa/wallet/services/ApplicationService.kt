aafffd982b596059e2bb51610fdee1f0ea78c714
Simone Infante <simone.infante@pagopa.it>
2024-02-26 13:06:33 +0100

    refactoring: service layer according new model

--- src/main/kotlin/it/pagopa/wallet/services/ApplicationService.kt
+++ src/main/kotlin/it/pagopa/wallet/services/ApplicationService.kt
@@ -1,70 +1,64 @@
 package it.pagopa.wallet.services
 
-import it.pagopa.wallet.audit.LoggedAction
-import it.pagopa.wallet.audit.ServiceCreatedEvent
-import it.pagopa.wallet.audit.ServiceStatusChangedEvent
-import it.pagopa.wallet.domain.services.ServiceId
-import it.pagopa.wallet.domain.services.ServiceName
-import it.pagopa.wallet.domain.services.ServiceStatus
-import it.pagopa.wallet.exception.ServiceNotFoundException
-import it.pagopa.wallet.repositories.ServiceRepository
+import it.pagopa.generated.wallet.model.ServiceStatusDto
+import it.pagopa.wallet.audit.*
+import it.pagopa.wallet.domain.applications.ApplicationDescription
+import it.pagopa.wallet.domain.applications.ApplicationId
+import it.pagopa.wallet.domain.applications.ApplicationStatus
+import it.pagopa.wallet.exception.ApplicationNotFoundException
+import it.pagopa.wallet.repositories.ApplicationRepository
 import java.time.Instant
-import java.util.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
-class ServicesService(@Autowired private val serviceRepository: ServiceRepository) {
-    fun createService(
-        serviceId: UUID,
-        serviceName: ServiceName,
-        status: ServiceStatus
-    ): Mono<LoggedAction<it.pagopa.wallet.domain.services.Service>> {
-        val service =
-            it.pagopa.wallet.domain.services.Service(
-                id = ServiceId(serviceId),
-                name = serviceName,
-                status = status,
+class ApplicationService(@Autowired private val applicationRepository: ApplicationRepository) {
+    fun createApplication(
+        applicationId: String,
+        status: ServiceStatusDto
+    ): Mono<LoggedAction<it.pagopa.wallet.domain.applications.Application>> {
+        val application =
+            it.pagopa.wallet.domain.applications.Application(
+                id = ApplicationId(applicationId),
+                description = ApplicationDescription(""), // TODO handle according API refactoring
+                status = ApplicationStatus.valueOf((status ?: ServiceStatusDto.DISABLED).value),
+                creationDate = Instant.now(),
                 lastUpdated = Instant.now()
             )
 
-        return serviceRepository
-            .save(it.pagopa.wallet.documents.service.Service.fromDomain(service))
-            .map { LoggedAction(service, ServiceCreatedEvent(service.id.id, service.name.name)) }
+        return applicationRepository
+            .save(it.pagopa.wallet.documents.applications.Application.fromDomain(application))
+            .map { LoggedAction(application, ApplicationCreatedEvent(application.id.id)) }
     }
 
-    fun setServiceStatus(
-        serviceId: UUID,
-        status: ServiceStatus
-    ): Mono<LoggedAction<it.pagopa.wallet.domain.services.Service>> {
-        return serviceRepository
-            .findById(serviceId.toString())
-            .switchIfEmpty(Mono.error(ServiceNotFoundException(serviceId)))
+    fun setApplicationStatus(
+        applicationId: String,
+        status: ServiceStatusDto
+    ): Mono<LoggedAction<it.pagopa.wallet.domain.applications.Application>> {
+        return applicationRepository
+            .findById(applicationId)
+            .switchIfEmpty(Mono.error(ApplicationNotFoundException(applicationId)))
             .map {
                 it.copy(status = status.toString(), lastUpdated = Instant.now().toString()) to
-                    ServiceStatus.valueOf(it.status)
+                    ApplicationStatus.valueOf((status ?: ServiceStatusDto.DISABLED).value)
             }
-            .flatMap { (service, oldStatus) ->
-                serviceRepository.save(service).map { service to oldStatus }
+            .flatMap { (application, oldStatus) ->
+                applicationRepository.save(application).map { application to oldStatus }
             }
-            .map { (service, oldStatus) ->
-                it.pagopa.wallet.domain.services.Service(
-                    ServiceId(serviceId),
-                    ServiceName(service.name),
-                    ServiceStatus.valueOf(service.status),
-                    Instant.parse(service.lastUpdated)
+            .map { (application, oldStatus) ->
+                it.pagopa.wallet.domain.applications.Application(
+                    ApplicationId(applicationId),
+                    ApplicationDescription(""),
+                    ApplicationStatus.valueOf(application.status),
+                    Instant.parse(application.creationDate),
+                    Instant.parse(application.lastUpdated)
                 ) to oldStatus
             }
-            .map { (service, oldStatus) ->
+            .map { (application, oldStatus) ->
                 LoggedAction(
-                    service,
-                    ServiceStatusChangedEvent(
-                        serviceId,
-                        service.name.name,
-                        oldStatus,
-                        service.status
-                    )
+                    application,
+                    ApplicationStatusChangedEvent(applicationId, oldStatus, application.status)
                 )
             }
     }
