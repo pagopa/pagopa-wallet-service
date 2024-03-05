e6f61ddb4facb80336b2dc7df64fba36d26a022f
Pietro Tota <pietro.tota@pagopa.it>
2024-03-05 18:11:01 +0100

    feat: add logo util and configuration

---
+++ src/main/kotlin/it/pagopa/wallet/config/LogoConfig.kt
@@ -0,0 +1,34 @@
+package it.pagopa.wallet.config
+
+import it.pagopa.generated.wallet.model.WalletCardDetailsDto
+import it.pagopa.wallet.domain.wallets.details.WalletDetailsType
+import it.pagopa.wallet.util.LogoUtils
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.net.URI
+
+@Configuration
+class LogoConfig {
+
+    @Bean
+    fun walletLogoMapping(@Value("#{\${wallet.logo_mapping}}") logoMapping: Map<String, String>): Map<String, URI> {
+        val transformedLogoMapping = logoMapping.mapValues {
+            URI.create(it.value)
+        }
+        val expectedKeys =
+            (
+                    WalletCardDetailsDto.BrandEnum.values().map { it.toString() }
+                            +
+                            WalletDetailsType.values().filter { it != WalletDetailsType.CARDS }.map { it.toString() }
+                            + LogoUtils.UNKNOWN_LOGO_KEY
+                    ).toSet()
+        val missingKeys = expectedKeys.filter { !transformedLogoMapping.containsKey(it) }
+        check(missingKeys.isEmpty()) {
+            "Invalid logo configuration map, missing logo entries for the following keys: $missingKeys"
+        }
+        return transformedLogoMapping
+    }
+
+}
+
