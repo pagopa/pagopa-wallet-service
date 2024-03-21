59fac9734dbff004857fed1138af5e6ad7cc71e9
Gianluca Ciuffa <gianlucaciuffa@MacBook-Pro-di-Gianluca.local>
2024-03-22 00:53:23 +0100

    fix spotless

--- src/main/kotlin/it/pagopa/wallet/documents/applications/Application.kt
+++ src/main/kotlin/it/pagopa/wallet/documents/applications/Application.kt
@@ -4,20 +4,20 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document("applications")
 data class Application(
-        val id: String,
-        val description: String,
-        val status: String,
-        val creationDate: String,
-        val updateDate: String
+    val id: String,
+    val description: String,
+    val status: String,
+    val creationDate: String,
+    val updateDate: String
 ) {
     companion object {
         fun fromDomain(application: it.pagopa.wallet.domain.applications.Application): Application =
-                Application(
-                        application.id.id,
-                        application.description.description,
-                        application.status.name,
-                        application.creationDate.toString(),
-                        application.updateDate.toString()
-                )
+            Application(
+                application.id.id,
+                application.description.description,
+                application.status.name,
+                application.creationDate.toString(),
+                application.updateDate.toString()
+            )
     }
 }
