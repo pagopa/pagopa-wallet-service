99bb4e1b116cec8ac68aa6f10962500415049342
Pietro Tota <pietro.tota@pagopa.it>
2024-04-04 10:58:12 +0200

    fix: add junit tests for coverage + error message refactoring

--- src/main/kotlin/it/pagopa/wallet/exception/NpgApiKeyConfigurationException.kt
+++ src/main/kotlin/it/pagopa/wallet/exception/NpgApiKeyConfigurationException.kt
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus
 /** Exception thrown when NPG per PSP api key configuration cannot be successfully parsed */
 open class NpgApiKeyConfigurationException(
     message: String,
-) : ApiError("Error parsing NPG PSP api keys configuration, cause: $message") {
+) : ApiError("Npg api key configuration error: $message") {
     override fun toRestException(): RestApiException =
         RestApiException(
             HttpStatus.INTERNAL_SERVER_ERROR,
