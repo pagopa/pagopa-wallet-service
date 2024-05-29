119ee9128da13e23c0d5e71a0f20d7195f423b21
petretiandrea <petretiandrea@gmail.com>
2024-05-29 15:43:31 +0200

    fix spotless

--- src/main/kotlin/it/pagopa/wallet/documents/wallets/details/PayPalDetails.kt
+++ src/main/kotlin/it/pagopa/wallet/documents/wallets/details/PayPalDetails.kt
@@ -3,8 +3,11 @@ package it.pagopa.wallet.documents.wallets.details
 import it.pagopa.wallet.domain.wallets.details.MaskedEmail
 import it.pagopa.wallet.domain.wallets.details.PayPalDetails
 
-data class PayPalDetails(val maskedEmail: String?, val pspId: String, val pspBusinessName: String = "") :
-    WalletDetails<PayPalDetails> {
+data class PayPalDetails(
+    val maskedEmail: String?,
+    val pspId: String,
+    val pspBusinessName: String = ""
+) : WalletDetails<PayPalDetails> {
 
     override fun toDomain() =
         PayPalDetails(maskedEmail?.let { MaskedEmail(it) }, pspId, pspBusinessName)
