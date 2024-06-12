9a9a70fe843cc1551ffd09f4e3f659ccb5d4c314
petretiandrea <petretiandrea@gmail.com>
2024-06-12 18:04:11 +0200

    fix spotless

--- src/main/kotlin/it/pagopa/wallet/services/DomainEventDispatcherService.kt
+++ src/main/kotlin/it/pagopa/wallet/services/DomainEventDispatcherService.kt
@@ -10,11 +10,11 @@ import it.pagopa.wallet.config.properties.ExpirationQueueConfig
 import it.pagopa.wallet.domain.wallets.DomainEventDispatcher
 import it.pagopa.wallet.domain.wallets.WalletId
 import it.pagopa.wallet.util.TracingUtils
+import java.time.Duration
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
-import java.time.Duration
 
 @Component
 class DomainEventDispatcherService(
