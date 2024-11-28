713f9ddad22be58653a9f2087fc8b33dcce6a8d8
CianoDanilo <danilo.ciano@pagopa.it>
2024-11-28 10:53:38 +0100

    feat(logged-event): add logs

--- src/main/kotlin/it/pagopa/wallet/services/WalletEventSinksService.kt
+++ src/main/kotlin/it/pagopa/wallet/services/WalletEventSinksService.kt
@@ -1,7 +1,6 @@
 package it.pagopa.wallet.services
 
 import it.pagopa.wallet.audit.LoggedAction
-import it.pagopa.wallet.domain.wallets.Wallet
 import it.pagopa.wallet.repositories.LoggingEventRepository
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,21 +14,23 @@ import reactor.core.publisher.Sinks
 class WalletEventSinksService(
     @Autowired private val loggingEventRepository: LoggingEventRepository
 ) : ApplicationListener<ApplicationReadyEvent> {
-    val walletEventSink: Sinks.Many<LoggedAction<*>> =
-        Sinks.many().unicast().onBackpressureBuffer()
+    val walletEventSink: Sinks.Many<LoggedAction<*>> = Sinks.many().unicast().onBackpressureBuffer()
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    fun <T : Any> tryEmitEvent(loggedAction: LoggedAction<T>) : Mono<LoggedAction<T>> =
-         Mono.just { walletEventSink.tryEmitNext(loggedAction) }
+    fun <T : Any> tryEmitEvent(loggedAction: LoggedAction<T>): Mono<LoggedAction<T>> =
+        Mono.defer {
+                walletEventSink.tryEmitNext(loggedAction)
+                Mono.just(loggedAction)
+            }
+            .doOnNext { logger.info("Logging event emitted") }
             .doOnError { logger.error("Exception while processing wallet event: ", it) }
-            .map{loggedAction}
             .onErrorResume { Mono.just(loggedAction) }
-
 
     override fun onApplicationEvent(event: ApplicationReadyEvent) {
         walletEventSink
             .asFlux()
             .flatMap { it.saveEvents(loggingEventRepository) }
+            .doOnNext { logger.info("Logging event saved") }
             .doOnError { logger.error("Exception while processing wallet event: ", it) }
             .onErrorResume { Mono.empty() }
             .subscribe()
