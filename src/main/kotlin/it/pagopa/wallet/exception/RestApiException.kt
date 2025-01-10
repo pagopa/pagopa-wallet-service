7357dd6b40949b9d87f186c93c6adfafa53a8bd0
Alex Mannia <123939805+a-mannia@users.noreply.github.com>
2025-01-10 14:09:36 +0100

    fix(logging): add description RestApiException (#202)
    Co-authored-by: Alex Mannia <alex.mannia@bvtech.com>

--- src/main/kotlin/it/pagopa/wallet/exception/RestApiException.kt
+++ src/main/kotlin/it/pagopa/wallet/exception/RestApiException.kt
@@ -4,4 +4,4 @@ import org.springframework.http.HttpStatus
 
 /** Rest api exception, used to return an error specific HttpStatus and reason code to the caller */
 class RestApiException(val httpStatus: HttpStatus, val title: String, val description: String) :
-    RuntimeException(title) {}
+    RuntimeException("[$httpStatus] $title - $description") {}
